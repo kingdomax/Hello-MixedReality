77436276a08b16b3619c2d5e13a5c52b1a65e0eb
kingdomax <kingdomax@windowslive.com>
2023-12-12 22:53:29 +0100

    Animate robot

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,26 +3,32 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public GameObject Robot;
+    public GameObject CenterEyeAnchor; 
 
     private Rigidbody _robotRb;
-    private Vector3 _robotInitialPosition;
+    private CharacterController _robotController;
     private Quaternion _robotInitialRotation;
 
     void Start()
     {
         _robotRb = Robot.GetComponent<Rigidbody>();
-        _robotInitialPosition = Robot.transform.position;
+        _robotController = Robot.GetComponent<CharacterController>();
         _robotInitialRotation = Robot.transform.rotation;
     }
 
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.One)) // A button
+        // A button
+        if (OVRInput.GetDown(OVRInput.Button.One))
         {
+            _robotController.enabled = false;
+
             _robotRb.velocity = Vector3.zero;
             _robotRb.angularVelocity = Vector3.zero;
-            Robot.transform.position = _robotInitialPosition;
+            Robot.transform.position = CenterEyeAnchor.transform.position + CenterEyeAnchor.transform.forward;
             Robot.transform.rotation = _robotInitialRotation;
+
+            _robotController.enabled = true;
         }
     }
 }
